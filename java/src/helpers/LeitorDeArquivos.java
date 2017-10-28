d1f9ac389989c7e934bdbbd8b9b112a4a5cf3e5f
octaviocarpes <octavio.carpes10@gmail.com>
2017-10-28 16:35:17 -0200

    "Terminada a implementacao da classe LeitorDeArquivos e Terminada a Montagem da classe Grafo. Adicionados testes para ambas as classes."

---
+++ java/src/helpers/LeitorDeArquivos.java
@@ -0,0 +1,66 @@
+package helpers;
+
+import model.Grafo;
+
+import java.io.*;
+
+/**
+ * Created by octaviocarpes on 10/22/17.
+ */
+public class LeitorDeArquivos {
+
+    private BufferedReader meuLeitor;
+    private String nomeDepositador;
+    private String nomeBeneficiario;
+
+    public LeitorDeArquivos(String nomeDoArquivo) {
+        try {
+            meuLeitor = new BufferedReader(new FileReader(new File(nomeDoArquivo)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        nomeDepositador = "";
+        nomeBeneficiario = "";
+    }
+
+    public void leArquivo(Grafo grafoParaPopular){
+        String linha = "";
+        Integer quantidadeClientes = 0;
+        try {
+            quantidadeClientes = Integer.parseInt(meuLeitor.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            while((linha = meuLeitor.readLine()) != null && quantidadeClientes > 0){
+                String[] dados = linha.split(" ");
+
+                Integer numeroDaConta = Integer.parseInt(dados[0]);
+                String nomeCorrentista1 = dados[1];
+                String nomeCorrentista2 = dados[2];
+
+                grafoParaPopular.adicionaVertice(numeroDaConta,nomeCorrentista1,nomeCorrentista2);
+                quantidadeClientes--;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String transferencia = meuLeitor.readLine();
+            String[] dadosTransferencia = transferencia.split(" ");
+            nomeDepositador = dadosTransferencia[0];
+            nomeBeneficiario = dadosTransferencia[1];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNomeDepositador() {
+        return nomeDepositador;
+    }
+
+    public String getNomeBeneficiario() {
+        return nomeBeneficiario;
+    }
+}
---
+++ test/java/helpers/LeitorDeArquivosTest.java
@@ -0,0 +1,10 @@
+package helpers;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by octaviocarpes on 10/28/17.
+ */
+public class LeitorDeArquivosTest {
+
+}
